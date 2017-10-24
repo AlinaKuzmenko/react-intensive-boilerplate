67828825bfe0dba919adf64fe479a85a4d57ac66
AlinaKuzmenko <k.alina.2014@ukr.net>
2017-10-24 21:12:43 +0300

    add default image for poster, edit Movie markup and styles

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,14 +1,29 @@
 // Core
 import React, { Component } from 'react';
+import { string } from 'prop-types';
 
 // Instruments
+import poster from '../../theme/assets/default-poster.png';
 import Styles from './styles.scss';
 
 
 export default class Movie extends Component {
+    static propTypes = {
+        poster: string.isRequired,
+        name: string.isRequired,
+    }
+    static defaultProps = {
+      poster: poster,
+      name: 'Movie name',
+      description: 'There\'s no description for this movie yet',
+    }
     render () {
+        const { poster, name } = this.props;
         return (
-            <figure className = { Styles.movie } />
+            <figure className = { Styles.movie }>
+                <img src = { poster } alt = { `${ name } poster` }/>
+                <figcaption>{ name }</figcaption>
+            </figure>
         );
     }
 }
