84cf1d08ff3f09d1a623711d5129a488c74bfcdf
AlinaKuzmenko <k.alina.2014@ukr.net>
2017-10-24 22:30:09 +0300

    handle input and submit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,56 @@
 // Core
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 // Instruments
 import Styles from './styles.scss';
 
 
-const Header = () => (
-    <header className = { Styles.header }>
+export default class Header extends Component {
+  constructor () {
+    super();
+    this.handleQueryChange = ::this._handleQueryChange;
+    this.searchMovie = ::this._searchMovie;
+  }
+  state = {
+    query: 'Search...'
+  }
+  _searchMovie (event) {
+    const { query } = this.state;
+    event.preventDefault();
+    if (!query) {
+      return;
+    }
+    console.log('searchMovie');
+    this.textInput.value = '';
+    this.setState(() => ({
+      query: 'Search...'
+    }));
+  }
+  _handleQueryChange (event) {
+    const query = event.target.value;
+    this.setState(() => ({ query }));
+  }
+  render () {
+    const { query } = this.state;
+    return (
+      <header className = { Styles.header }>
         <h1>
-            <Link to = '/'>Moviesearcha</Link>
+          <Link to = '/'>Moviesearcha</Link>
         </h1>
-        <input type = 'text' />
-    </header>
-);
-
-export default Header;
+        <form onSubmit = { this.searchMovie } >
+          <input
+              onChange = { this.handleQueryChange }
+              type = 'text'
+              placeholder = { query }
+              ref = { (input) => { this.textInput = input } }
+          />
+          <input
+              type = 'submit'
+              value = 'Search'
+          />
+        </form>
+      </header>
+    );
+  }
+}
