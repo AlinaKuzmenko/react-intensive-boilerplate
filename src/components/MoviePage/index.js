2ad1aba543ca55d8e6f87f1fd56e7b2fd12b3eb3
AlinaKuzmenko <k.alina.2014@ukr.net>
2017-10-26 18:19:44 +0300

    style MoviePage

--- src/components/MoviePage/index.js
+++ src/components/MoviePage/index.js
@@ -20,14 +20,30 @@ export default class MoviePage extends Component {
     }
     render () {
         const { posterURL } = this.context;
-        const { movie } = this.props;
-
-        console.log('movie: ', movie);
+        const { movie: {
+            title,
+            poster_path,
+            original_language,
+            overview,
+            popularity,
+            release_date,
+            vote_average,
+        } } = this.props;
 
         return (
             <section className = { Styles.moviePage }>
-                <h2>{ movie.title }</h2>
-                <img alt = { `${movie.title} movie poster` } src = { `${posterURL}/${movie.poster_path}` } />
+                <h2>{ title }</h2>
+                <img alt = { `${title} movie poster` } src = { `${posterURL}/${poster_path}` } />
+                <div className = { Styles.details }>
+                    <p><b>Original language:</b> { original_language }</p>
+                    <p><b>Popularity:</b> { popularity }</p>
+                    <p><b>Release date:</b> { release_date }</p>
+                    <p><b>Vote Average:</b> { vote_average }</p>
+                </div>
+                <section className = { Styles.overview }>
+                    <h5>Overview</h5>
+                    <p>{ overview }</p>
+                </section>
             </section>
         );
     }
