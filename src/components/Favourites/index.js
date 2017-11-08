e196c4ed579f2e91ad7de10f5c3a41efc98c9aba
AlinaKuzmenko <k.alina.2014@ukr.net>
2017-11-08 21:55:24 +0200

    add animations to list of fav

--- src/components/Favourites/index.js
+++ src/components/Favourites/index.js
@@ -1,10 +1,9 @@
 // Core
 import React, { Component } from 'react';
 import { array, string } from 'prop-types';
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 // Instruments
-import { getUniqueID } from '../../helpers';
 import Styles from './styles.scss';
 
 
@@ -27,28 +26,42 @@ export default class Favourites extends Component {
             ? movies.map(
                 ({ id, poster_path: posterPath, title }) => ( // eslint-disable-line
                     <CSSTransition
-                        classNames = {{
-                            appear: Styles.liAppear,
+                        classNames = { {
+                            appear:       Styles.liAppear,
                             appearActive: Styles.liAppearActive,
-                            enter: Styles.liEnter,
-                            enterActive: Styles.liEnterActive,
-                            exit: Styles.liExit,
-                            exitActive: Styles.liExitActive,
-                        }}
+                            enter:        Styles.liEnter,
+                            enterActive:  Styles.liEnterActive,
+                            exit:         Styles.liExit,
+                            exitActive:   Styles.liExitActive
+                        } }
                         key = { id }
-                        timeout = {{
-                            enter: 500,
-                            exit: 500
-                        }}
-                    >
-                        <li key = { getUniqueID(15) }>
-                            <img alt = {`${title} poster`} src = { `${posterURL}/${posterPath}` } />
+                        timeout = { 300 }>
+                        <li>
+                            <img
+                                alt = { `${title} poster` }
+                                src = { `${posterURL}/${posterPath}` }
+                            />
                             <span>{ title }</span>
                         </li>
                     </CSSTransition>
                 )
             )
-            : <li>No movies</li>;
+            : (<CSSTransition
+                classNames = { {
+                    appear:       Styles.noMoviesAppear,
+                    appearActive: Styles.noMoviesAppearActive,
+                    enter:        Styles.noMoviesEnter,
+                    enterActive:  Styles.noMoviesEnterActive,
+                    exit:         Styles.noMoviesExit,
+                    exitActive:   Styles.noMoviesExitActive
+                } }
+                timeout = { {
+                    enter: 500,
+                    exit:  0
+                } }>
+                <li>No movies</li>
+            </CSSTransition>
+            );
 
         return (
             <aside className = { Styles.favourites }>
