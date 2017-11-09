7e3c7ea2f5c7b6814e5b8ee51512735ec7f0a299
AlinaKuzmenko <k.alina.2014@ukr.net>
2017-11-09 03:06:18 +0200

    add animation for Modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { func, object, string } from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import Styles from './styles.scss';
 
@@ -57,47 +58,63 @@ export default class Modal extends Component {
         const src = `${posterURL}${poster}`;
 
         return (
-            <section
-                className = { Styles.modal }
-                id = { `modal-${id}` }
-                style = { { top } }>
-                <header>
-                    <h2>{ title }</h2>
-                    <span
-                        className = { Styles.close }
-                        onClick = { this.handleModal }>
-                        X
-                    </span>
-                </header>
-                <div className = { Styles.details }>
-                    <img
-                        alt = { `${title} poster` }
-                        src = { src }
-                    />
-                    <div className = { Styles.info }>
-                        <p>
-                            <mark>original title:</mark>
-                            <span>{ title }</span>
-                        </p>
-                        <p>
-                            <mark>release date:</mark>
-                            <span>{ releaseDate }</span>
-                        </p>
-                        <p>
-                            <mark>popularity:</mark>
-                            <span>{ popularity }</span>
-                        </p>
-                        <p>
-                            <mark>votes:</mark>
-                            <span>{ votes }</span>
-                        </p>
-                        <p>
-                            <mark>overview:</mark>
-                            <span>{ overview }</span>
-                        </p>
-                    </div>
-                </div>
-            </section>
+            <TransitionGroup>
+                <CSSTransition
+                    appear
+                    classNames = { {
+                        appear:       Styles.modalAppear,
+                        appearActive: Styles.modalAppearActive,
+                        enter:        Styles.modalEnter,
+                        enterActive:  Styles.modalEnterActive,
+                        exit:         Styles.modalExit,
+                        exitActive:   Styles.modalExitActive
+                    } }
+                    timeout = { 700 }
+                    key = {id}
+                >
+                    <section
+                        className = { Styles.modal }
+                        id = { `modal-${id}` }
+                        style = { { top } }>
+                        <header>
+                            <h2>{ title }</h2>
+                            <span
+                                className = { Styles.close }
+                                onClick = { this.handleModal }>
+                                X
+                            </span>
+                        </header>
+                        <div className = { Styles.details }>
+                            <img
+                                alt = { `${title} poster` }
+                                src = { src }
+                            />
+                            <div className = { Styles.info }>
+                                <p>
+                                    <mark>original title:</mark>
+                                    <span>{ title }</span>
+                                </p>
+                                <p>
+                                    <mark>release date:</mark>
+                                    <span>{ releaseDate }</span>
+                                </p>
+                                <p>
+                                    <mark>popularity:</mark>
+                                    <span>{ popularity }</span>
+                                </p>
+                                <p>
+                                    <mark>votes:</mark>
+                                    <span>{ votes }</span>
+                                </p>
+                                <p>
+                                    <mark>overview:</mark>
+                                    <span>{ overview }</span>
+                                </p>
+                            </div>
+                        </div>
+                    </section>
+                </CSSTransition>
+            </TransitionGroup>
         );
     }
 }
